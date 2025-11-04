19a899830d984c8bbf7eb5f1f76a1f97e8bc55ab
jamesdee0904 <24-37104@g.batstate-u.edu.ph>
2025-11-04 14:57:45 +0800

    Delete Advance Computer Programming directory

--- Advance Computer Programming/cms/cms/static/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById("learnMoreBtn");
-    if (button) {
-        button.addEventListener("click", () => {
-            alert("You are learning Flask step by step!");
-        });
-    }
-});
